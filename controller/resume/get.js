12ee5eb7270039ee15561259733c8c9ab051d5cb
anasmasti <anasmasti@hotmail.com>
2021-06-17 16:42:25 +0100

    Add download resum functionality

---
+++ controller/resume/get.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+    getResume: (req, res) => {
+        try {
+            const resumeFile = path.join('resources', 'masti_anas_cv.pdf');
+            // Download the resume
+            return res.download(resumeFile);
+
+        } catch {
+            res.statut(422).send('An error occurred, please try again later.')
+        }
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const Contact = require('../controller/contact/post')
-const Home = require('../controller/home/get')
+const postContact = require('../controller/contact/post')
+const getHome = require('../controller/home/get')
+const getResume = require('../controller/resume/get')
 
 const router = express.Router()
 
 /////// Global routers /////// 
 // Home route
-router.route('/').get(Home.getHome); // Get home
+router.route('/').get(getHome.getHome); // Get home
 // Contact route
-router.route('/contact').post(Contact.postContact); // Post contact
+router.route('/contact').post(postContact.postContact); // Post contact
+// Resume route
+router.route('/resume').get(getResume.getResume)
 
 module.exports = router
